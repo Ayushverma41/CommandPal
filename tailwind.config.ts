1173dfff52fbc311a7283bded26f6cc399480d50
Ayush Verma <ayush21032016@gmail.com>
2025-12-08 04:27:10 +0000

    layout
    import type { Metadata } from 'next';
    import './globals.css';
    im

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['var(--font-inter)', 'sans-serif'],
-        headline: ['var(--font-inter)', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['"Source Code Pro"', 'monospace'],
       },
       colors: {
